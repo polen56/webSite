9c703e9b2c38947bba1d2416412dfcccfec87580
Stéphane GRILLON <sgrillon@hotmail.fr>
2021-03-11 14:33:23 +0100

    linter

--- polen-front/src/app/core/utils/moment-date-formatter.ts
+++ polen-front/src/app/core/utils/moment-date-formatter.ts
@@ -1,4 +1,4 @@
-import { NgbDateParserFormatter, NgbDateStruct } from "@ng-bootstrap/ng-bootstrap";
+import { NgbDateParserFormatter, NgbDateStruct } from '@ng-bootstrap/ng-bootstrap';
 import * as moment from 'moment';
 
 export class MomentDateFormatter extends NgbDateParserFormatter {
@@ -8,14 +8,18 @@ export class MomentDateFormatter extends NgbDateParserFormatter {
     parse(value: string): NgbDateStruct {
         if (value) {
             value = value.trim();
-            let mdt = moment(value, this.DT_FORMAT)
+            const mdt = moment(value, this.DT_FORMAT);
         }
         return null;
     }
     format(date: NgbDateStruct): string {
-        if (!date) return '';
-        let mdt = moment([date.year, date.month - 1, date.day]);
-        if (!mdt.isValid()) return '';
+        if (!date) {
+          return '';
+        }
+        const mdt = moment([date.year, date.month - 1, date.day]);
+        if (!mdt.isValid()) {
+          return '';
+        }
         return mdt.format(this.DT_FORMAT);
     }
 }
